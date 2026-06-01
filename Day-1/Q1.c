c53d89a3e22d8359c3862a69ac3e559671be16ae
ADITYA <raushankumarsahu@Adityas-MacBook-Air.local>
2026-06-01 10:52:32 +0530

    Day-1 assignment is uploaded.

---
+++ Day-1/Q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i,sum=0;
+    printf("Enter the number:");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("the sum of First %d natural number is %d", n,sum);
+    return 0;
+}
